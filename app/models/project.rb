c14a9e2a731b01af1e183d2a5c4916b82e343e6f
Aaron Trostle <aaron.trostle@gmail.com>
2013-05-19 21:04:27 -0700

    Make projects only visible to users with permission to see them

--- app/models/project.rb
+++ app/models/project.rb
@@ -2,4 +2,10 @@ class Project < ActiveRecord::Base
   attr_accessible :name, :description
   has_many :tickets, :dependent => :delete_all
   validates :name, :presence => true
+
+  has_many :permissions, :as => :thing
+  def self.viewable_by(user)
+    joins(:permissions).where(:permissions => { :action => "view",
+                                                :user_id => user.id })
+  end
 end
