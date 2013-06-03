e960e3eba6621b7798b3d24ddf66153071603e77
Aaron Trostle <aaron.trostle@gmail.com>
2013-06-03 14:46:48 -0700

    Add the ability to attach a file to a ticket

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -2,7 +2,9 @@ class Ticket < ActiveRecord::Base
   belongs_to :project
   belongs_to :user
 
-  attr_accessible :title, :description
+  attr_accessible :title, :description, :asset
+
+  has_attached_file :asset
 
   validates :title, :presence => true
   validates :description, :presence => true, :length => { :minimum => 10 }
